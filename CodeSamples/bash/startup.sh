6632baf3e0c626c4b84372c14547cdc42ccda277
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2022-07-13 17:04:20 -0700

    made sure to verify the hashes

--- CodeSamples/bash/startup.sh
+++ CodeSamples/bash/startup.sh
@@ -1,6 +1,6 @@
 sudo apt update;
 sudo apt upgrade;
-sudo apt install tmux htop git nodejs python3 python3-pip tree wget nmap man-db info nano build-essential xxd ssh;
+sudo apt install tmux htop git nodejs python3 python3-pip tree wget nmap man-db info nano build-essential xxd ssh curl;
 sudo apt autoremove;
 sudo apt autoclean;
 cd ~;
@@ -8,11 +8,19 @@ mkdir bin
 cd ~/bin
 wget https://github.com/arl/gitmux/releases/download/v0.7.9/gitmux_0.7.9_linux_amd64.tar.gz;
 wget https://github.com/arl/gitmux/releases/download/v0.7.9/checksums.txt;
-let h=`sha256 gitmux_0.7.9_linux_amd64.tar.gz`
-
-tar -xf gitmux-0.7.9_linux_amd64.tar.gz;
-export PATH=$PATH:`pwd`;
-cd ~;
-curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bash_aliases > ~/.bash_aliases
-curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.tmux.conf > ~/.tmux.conf
-curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bashrc > ~/.bashrc
+let hash256=`sha256sum gitmux_0.7.9_linux_amd64.tar.gz`
+if grep -Fxq "$hash256" checksums.txt
+then
+    tar -xf gitmux-0.7.9_linux_amd64.tar.gz;
+    export PATH=$PATH:`pwd`;
+    cd ~;
+    curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bash_aliases > ~/.bash_aliases
+    curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.tmux.conf > ~/.tmux.conf
+    curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bashrc > ~/.bashrc
+else
+    echo "hash does not match";
+    echo "Got:";
+    echo "$hash256";
+    echo "Expected one of:";
+    cat checksums.txt
+fi
