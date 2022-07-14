13664aacc1b0698955e3c6bc37f65e94e70eab55
ErikRospo <87732519+ErikRospo@users.noreply.github.com>
2022-07-14 08:39:24 -0700

    Update startup.sh

--- CodeSamples/bash/startup.sh
+++ CodeSamples/bash/startup.sh
@@ -7,20 +7,9 @@ cd ~;
 mkdir bin
 cd ~/bin
 wget https://github.com/arl/gitmux/releases/download/v0.7.9/gitmux_0.7.9_linux_amd64.tar.gz;
-wget https://github.com/arl/gitmux/releases/download/v0.7.9/checksums.txt;
-let hash256=`sha256sum gitmux_0.7.9_linux_amd64.tar.gz`
-if grep -Fxq "$hash256" checksums.txt
-then
-    tar -xf gitmux-0.7.9_linux_amd64.tar.gz;
-    export PATH=$PATH:`pwd`;
-    cd ~;
-    curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bash_aliases > ~/.bash_aliases
-    curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.tmux.conf > ~/.tmux.conf
-    curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bashrc > ~/.bashrc
-else
-    echo "hash does not match";
-    echo "Got:";
-    echo "$hash256";
-    echo "Expected one of:";
-    cat checksums.txt
-fi
+tar -xf gitmux-0.7.9_linux_amd64.tar.gz;
+export PATH=$PATH:`pwd`;
+cd ~;
+curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bash_aliases > ~/.bash_aliases
+curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.tmux.conf > ~/.tmux.conf
+curl https://raw.githubusercontent.com/ErikRospo/Resources/main/CodeSamples/bash/.bashrc > ~/.bashrc
